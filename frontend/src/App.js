6b31a07821d1b0a3cad8346873403382a1f09f4e
PranayJadhao <pranayjadhao95@gmail.com>
2023-11-07 20:16:00 +0530

    adding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import MovieCard from "./Components/MovieCart";
-import { Input } from "@chakra-ui/react";
 
 function App() {
   //Managing movies and queries using useState hook
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [search, setSearch] = useState("");
 
   //Calling fetchData function to get data from api whenerver query changes
   useEffect(() => {
     async function Fetchdata() {
       try {
         let res = await fetch(
-          `https://www.omdbapi.com/?t=${searchQuery || "batman"}&apikey=bbe6805d`
+          `https://www.omdbapi.com/?s=${
+            searchQuery || "batman"
+          }&apikey=bbe6805d`
         );
         var data = await res.json();
-        console.log(data);
-        setMovies(data);
+
+        setMovies(data.Search);
       } catch (err) {
         console.log(err);
       }
@@ -25,18 +27,31 @@ function App() {
     Fetchdata();
   }, [searchQuery]);
 
+  const handleSearch = () => {
+    setSearchQuery(search);
+    console.log(search);
+  };
+
   return (
     <div className="App">
       <h1>Movie App</h1>
-     
-      <Input
-        placeholder="Search for movie..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      {console.log(searchQuery)}
 
-      <MovieCard prop={movies}></MovieCard>
+      <input
+        placeholder="Search for a movie..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button id="searchButton" onClick={handleSearch}>
+        Search
+      </button>
+
+      <div id="box">
+        {movies.length === 1 ? (
+          <MovieCard prop={movies[0]} />
+        ) : (
+          movies.map((movie, i) => <MovieCard key={i} prop={movie} />)
+        )}
+      </div>
     </div>
   );
 }
--- frontend/src/Components/MovieCart.js
+++ frontend/src/Components/MovieCart.js
@@ -2,42 +2,47 @@ import { Button, Image } from "@chakra-ui/react";
 import React, { useState } from "react";
 import "./MovieCart.css";
 
-
 const MovieCard = ({ prop }) => {
   const [flag, setFlag] = useState(false);
 
- 
   return (
+    <div id="outerBox">
       <div>
+        <Image
+          id="image"
+          style={{ width: "150px", padding: "10px" }}
+          src={prop.Poster}
+          alt="Poster Not Found"
+        />
+      </div>
+      <div className="details">
         <div>
-          <Image
-            id="image"
-            style={{ width: "150px", padding: "10px" }}
-            src={prop.Poster}
-            alt="Poster Not Found"
-          />
-        </div>
-
-        <div className="details">
-          <div> <b>Title: {prop.Title}</b></div>
-        </div>
-
-        <div>
-        
-          <div>
-{
-        flag ? <div className="details">`<b>Director:</b>  {prop.Director}<br/>
-        <b>Actors:</b>{prop.Actors}<br/>
-        <b>Awards:</b>{prop.Awards}<br/>
-        <b>Year: </b>{prop.Year}<br/>
-        <b>Genre: </b> {prop.Genre}<br/>
-        <b>IMDB Rating:</b> {prop.imdbRating}<br/>
-        `</div>  :<Button onClick={()=>setFlag(true)}>See More</Button>
-       }
-          </div>
+          {" "}
+          <b>Title: {prop.Title}</b>
         </div>
       </div>
 
+      <div>
+        <div>
+          {flag ? (
+            <div className="details">
+              `<b>Director:</b> {prop.Director}
+              <br />
+              <b>Type:</b>
+              {prop.Type}
+              <br />
+              <b>Year: </b>
+              {prop.Year}
+              <br />`
+            </div>
+          ) : (
+            <Button id="moreButton" onClick={() => setFlag(true)}>
+              See More
+            </Button>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
